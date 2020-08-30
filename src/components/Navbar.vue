3e0c244e53b5bd2ffb3db9e6da0cfb86ea5f3b99
= <krishnajha366@gmail.com>
2020-08-30 23:27:28 +0530

    Navbar updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -23,14 +23,11 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-    
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn text>
+        
+        <span class="ques-btn"><router-link to="/addQuestion"> Add Question </router-link></span>
+        <!-- <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon> -->
       </v-btn>
     </v-app-bar>
 
@@ -43,5 +40,10 @@ export default {
 </script>
 
 <style scoped>
+
+span a{
+  color: #ffffff;
+  text-decoration: none;
+}
 
 </style>
