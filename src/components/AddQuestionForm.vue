71abf8e1c0b10e31913c764d63f404c1e1b0fdb7
= <krishnajha366@gmail.com>
2020-08-30 22:12:49 +0530

    First commit

---
+++ src/components/AddQuestionForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="form-container">
+      <form action="" method="post">
+          <div class="form-group">
+              <label for=""></label>
+              <input type="text" name="" id="">
+          </div>
+          <div class="form-group">
+              
+          </div>
+          <div class="form-group">
+              
+          </div>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
